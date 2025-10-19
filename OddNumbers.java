b307f68845368e5ea862e88e37a1f929c0040a03
aaronmeastwood <227181718+aaronmeastwood-code@users.noreply.github.com>
2025-10-19 21:29:51 +1000

    Add OddNumbers

---
+++ OddNumbers.java
@@ -0,0 +1,10 @@
+public class OddNumbers {
+    public static void main(String[] args) {
+        System.out.println("Odd numbers from 1 to 99:");
+        for (int i = 1; i <= 50; i++) {
+            if (i % 2 == 1)
+                System.out.print(i + " ");
+        }
+    }
+}
+
